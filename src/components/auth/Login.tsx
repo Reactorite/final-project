4ed9df8e7df79040163c2a4a6ac93cb30d2e8ec6
Berkan Karaveli <berkankaraveli@abv.bg>
2024-08-19 14:05:23 +0300

    Login

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,59 @@
+import { useContext, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { loginUser } from '../../services/auth.service';
+import { AppContext } from '../../state/app.context';
+import { UserDataType } from '../../types/UserDataType'; 
+import React from 'react';
+
+export default function Login() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { setAppState } = useContext(AppContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const updateUser = (prop: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({
+      ...user,
+      [prop]: e.target.value,
+    });
+  };
+
+  const login = async () => {
+    if (!user.email || !user.password) {
+      return alert('No credentials provided!');
+    }
+
+    try {
+      const userData: UserDataType = await loginUser(user.email, user.password);
+
+      setAppState((prevState) => ({
+        ...prevState,
+        user: userData, 
+        userData: null,
+      }));
+
+      navigate(location.state?.from.pathname ?? '/');
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        alert('An unexpected error occurred.');
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <label htmlFor="email">Email: </label>
+      <input value={user.email} onChange={updateUser('email')} type="text" name="email" id="email" /><br /><br />
+      <label htmlFor="password">Password: </label>
+      <input value={user.password} onChange={updateUser('password')} type="password" name="password" id="password" /><br />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
