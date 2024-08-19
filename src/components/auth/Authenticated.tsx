81c2ca50a0c9d12ff8c1b65be341c18dce0a7648
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-08-19 09:02:49 +0100

    Moved Authenticated inside auth folder

--- src/components/auth/Authenticated.tsx
+++ src/components/auth/Authenticated.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, useContext } from "react";
-import { AppContext } from "../state/app.context";
+import { AppContext } from "../../state/app.context";
 import { Navigate, useLocation } from "react-router";
 
 interface AuthenticatedProps {
