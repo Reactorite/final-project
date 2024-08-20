524b0d538e5cca9be48be25912765e989f9b851b
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-08-20 06:55:54 +0100

    Minor bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppContext, AppContextType } from './state/app.context';
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NavLink } from "react-router-dom";
 import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../state/app.context";
