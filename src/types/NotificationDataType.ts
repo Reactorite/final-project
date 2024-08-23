83b5ddef73592fe74035c76c325c7ad6d521ae3c
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-08-23 11:48:39 +0100

    Added comments for possible enums

--- src/types/NotificationDataType.ts
+++ src/types/NotificationDataType.ts
@@ -6,3 +6,35 @@ export interface NotificationDataType {
     status: "unread" | "read";
     invitationStatus?: "pending" | "accepted" | "rejected";
   }
+
+  // const ROLE = {
+  //   OWNER: "OWNER",
+  //   ADMIN: "ADMIN",
+  //   STUDENT: "STUDENT",
+  //   TEACHER: "TEACHER",
+  // } as const;
+  
+  // interface User {
+  //   roles: keyof typeof ROLE[];
+  // }
+  // const ROLE = {
+  //   OWNER: "OWNER",
+  //   ADMIN: "ADMIN",
+  //   STUDENT: "STUDENT",
+  //   TEACHER: "TEACHER",
+  // } as const;
+  
+  // interface User {
+  //   roles: keyof typeof ROLE[];
+  // }
+
+  // enum ROLE {
+  //   OWNER = "OWNER",
+  //   ADMIN = "ADMIN",
+  //   STUDENT = "STUDENT",
+  //   TEACHER ="TEACHER",
+  // };
+  
+  // interface User {
+  //   roles: ROLE[];
+  // }
