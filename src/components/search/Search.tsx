639a8615546b6b4ceb766e750792f242da58c476
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-08-24 09:34:23 +0100

    Implemented search for quizzes

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,15 +2,16 @@ import React, { useEffect, useRef, useState } from "react";
 import { UserDataType } from "../../types/UserDataType";
 import { onValue, ref } from "firebase/database";
 import { db } from "../../config/firebase-config";
+import QuizDataType from "../../types/QuizDataType";
 
 interface SearchProps {
   searchQuery: string;
   searchCategory: string;
   handleCloseSearch: () => void;
-} 
+}
 
 const Search: React.FC<SearchProps> = ({ searchQuery, searchCategory, handleCloseSearch }) => {
-  const [results, setResults] = useState<UserDataType[]>([]);
+  const [results, setResults] = useState<(UserDataType | QuizDataType)[]>([]);
   const searchRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -25,6 +26,17 @@ const Search: React.FC<SearchProps> = ({ searchQuery, searchCategory, handleClos
           setResults(filteredResults);
         }
       });
+    } else if (searchCategory === "iQuizzes") {
+      const quizesRef = ref(db, "quizzes");
+      onValue(quizesRef, (snapshot) => {
+        const response: Record<string, QuizDataType> | null = snapshot.val();
+        if (response) {
+          const filteredResults = Object.values(response).filter((quiz) =>
+            quiz.title.toLowerCase().includes(searchQuery.toLowerCase())
+          );
+          setResults(filteredResults);
+        }
+      })
     }
   }, [searchQuery, searchCategory]);
 
@@ -41,30 +53,42 @@ const Search: React.FC<SearchProps> = ({ searchQuery, searchCategory, handleClos
     };
   }, [handleCloseSearch]);
 
+  const isUserDataType = (item: UserDataType | QuizDataType): item is UserDataType => {
+    return ((item as UserDataType).username !== undefined);
+  };
+
   return (
     <div ref={searchRef}>
-      <h4 style={{ position: "sticky", top: "0", background: "white", zIndex: 1, borderBottom: "1px solid #0D6EFD" }}>Search Results 
+      <h4 style={{ position: "sticky", top: "0", background: "white", zIndex: 1, borderBottom: "1px solid #0D6EFD" }}>Search Results
         {<button
-        onClick={handleCloseSearch}
-        style={{
-          position: "absolute",
-          right: "10px",
-          top: "-18px",
-          // border: "2px solid #0D6EFD",
-          background: "none",
-          cursor: "pointer",
-          fontSize: "30px",
-          color: "#0D6EFD"
-        }}
-      >
-        &times;
-      </button>}</h4>
+          onClick={handleCloseSearch}
+          style={{
+            position: "absolute",
+            right: "10px",
+            top: "-18px",
+            // border: "2px solid #0D6EFD",
+            background: "none",
+            cursor: "pointer",
+            fontSize: "30px",
+            color: "#0D6EFD"
+          }}
+        >
+          &times;
+        </button>}
+      </h4>
       {results.length > 0 ? (
-        results.map((user, index) => (
-          <div key={index}>
-            <p>Username: {user.username}</p>
-            <p>Global Points: {user.globalPoints}</p>
-          </div>
+        results.map((item, index) => (
+          isUserDataType(item) ? (
+            <div key={index}>
+              <p>Username: {item.username}</p>
+              <p>Global Points: {item.globalPoints}</p>
+            </div>
+          ) : (
+            <div key={index}>
+              <p>Title: {item.title}</p>
+              <p>Category: {item.category}</p>
+            </div>
+          )
         ))
       ) : (
         <p>No results found</p>
